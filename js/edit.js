866b88c6946b3d04f174554fa9737b79e0ed6057
Venkatasubramanian B <bvenkysubbu@yahoo.com>
2018-07-04 06:21:26 +0530

    Change as prep for the share feature

--- js/edit.js
+++ js/edit.js
@@ -1,35 +1,48 @@
 document.oncontextmenu = function() {
     return false;
 }
+var currentScore1;
+var currentScore2;
+var currentSet1;
+var currentSet2;
+
 function increaseScore1() {
-    var currentScore1 = parseInt(document.getElementById("score1").innerHTML);
-    document.getElementById("score1").innerHTML = currentScore1 + 1;
+    currentScore1 = parseInt(document.getElementById("score1").innerHTML);
+    currentScore1++;
+    document.getElementById("score1").innerHTML = currentScore1;
 }
 function decreaseScore1() {
-    var currentScore1 = parseInt(document.getElementById("score1").innerHTML);
-    document.getElementById("score1").innerHTML = currentScore1 - 1;
+    currentScore1 = parseInt(document.getElementById("score1").innerHTML);
+    currentScore1--;
+    document.getElementById("score1").innerHTML = currentScore1;
 }
 function increaseScore2() {
-    var currentScore2 = parseInt(document.getElementById("score2").innerHTML);
-    document.getElementById("score2").innerHTML = currentScore2 + 1;
+    currentScore2 = parseInt(document.getElementById("score2").innerHTML);
+    currentScore2++;
+    document.getElementById("score2").innerHTML = currentScore2;
 }
 function decreaseScore2() {
-    var currentScore2 = parseInt(document.getElementById("score2").innerHTML);
-    document.getElementById("score2").innerHTML = currentScore2 - 1;
+    currentScore2 = parseInt(document.getElementById("score2").innerHTML);
+    currentScore2--;
+    document.getElementById("score2").innerHTML = currentScore2;
 }
 function increaseSet1() {
-    var currentSet1 = parseInt(document.getElementById("set1").innerHTML);
-    document.getElementById("set1").innerHTML = currentSet1 + 1;
+    currentSet1 = parseInt(document.getElementById("set1").innerHTML);
+    currentSet1++;
+    document.getElementById("set1").innerHTML = currentSet1;
 }
 function decreaseSet1() {
-    var currentSet1 = parseInt(document.getElementById("set1").innerHTML);
-    document.getElementById("set1").innerHTML = currentSet1 - 1;
+    currentSet1 = parseInt(document.getElementById("set1").innerHTML);
+    currentSet1--;
+    document.getElementById("set1").innerHTML = currentSet1;
 }
 function increaseSet2() {
-    var currentSet2 = parseInt(document.getElementById("set2").innerHTML);
-    document.getElementById("set2").innerHTML = currentSet2 + 1;
+    currentSet2 = parseInt(document.getElementById("set2").innerHTML);
+    currentSet2++;
+    document.getElementById("set2").innerHTML = currentSet2;
 }
 function decreaseSet2() {
-    var currentSet2 = parseInt(document.getElementById("set2").innerHTML);
-    document.getElementById("set2").innerHTML = currentSet2 - 1;
+    currentSet2 = parseInt(document.getElementById("set2").innerHTML);
+    currentSet2--;
+    document.getElementById("set2").innerHTML = currentSet2;
 }
